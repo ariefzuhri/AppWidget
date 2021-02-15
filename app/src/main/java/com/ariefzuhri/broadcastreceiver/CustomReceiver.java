ebd416d8650eca994bd5c6ab0f227754383e0962
ariefzuhri <ariefzuhri72@gmail.com>
2021-02-15 14:12:17 +0700

    Initial commit

---
+++ app/src/main/java/com/ariefzuhri/broadcastreceiver/CustomReceiver.java
@@ -0,0 +1,35 @@
+package com.ariefzuhri.broadcastreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+//Enabled: Bisa inisiasi receiver/memfungsikan
+//Exported: Bisa menerima receiver dari luar
+public class CustomReceiver extends BroadcastReceiver {
+    private static final String ACTION_CUSTOM_BROADCAST =
+            BuildConfig.APPLICATION_ID + ".ACTION_CUSTOM_BROADCAST";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String intentAction = intent.getAction();
+        if (intentAction != null){
+            String toastMessage = "";
+            switch (intentAction){
+                case Intent.ACTION_POWER_CONNECTED:
+                    toastMessage = context.getString(R.string.power_connected);
+                    break;
+
+                case Intent.ACTION_POWER_DISCONNECTED:
+                    toastMessage = context.getString(R.string.power_disconnected);
+                    break;
+
+                case ACTION_CUSTOM_BROADCAST:
+                    toastMessage = intent.getStringExtra("DATA");
+                    break;
+            }
+            Toast.makeText(context, toastMessage, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
